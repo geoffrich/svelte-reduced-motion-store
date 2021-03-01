3f197851917c2106877ca396991d1bdb224d3a4b
Geoff Rich <geoff.rich@alaskaair.com>
2021-02-28 16:31:01 -0800

    Initial store implementation

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script>
+	import reducedMotion from './reducedMotion';
+
 	export let name;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>reduced motion: {$reducedMotion}</p>
 </main>
 
 <style>
---
+++ src/reducedMotion.js
@@ -0,0 +1,24 @@
+import { readable } from 'svelte/store';
+
+const reducedMotionQuery = '(prefers-reduced-motion: no-preference)';
+
+function getInitialMotionPreference() {
+	const hasNoMotionPreference = window.matchMedia(reducedMotionQuery).matches;
+	return !hasNoMotionPreference;
+}
+
+export default readable(false, (set) => {
+	const mediaQueryList = window.matchMedia(reducedMotionQuery);
+
+	const updateMotionPreference = (event) => {
+		set(!event.matches);
+	};
+
+	mediaQueryList.addEventListener('change', updateMotionPreference);
+
+	set(getInitialMotionPreference());
+
+	return () => {
+		mediaQueryList.removeEventListener('change', updateMotionPreference);
+	}
+});
