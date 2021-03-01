c0455d54f23cff70233c852f64a0c294ac3f7813
Geoff Rich <geoff.rich@alaskaair.com>
2021-02-28 17:04:42 -0800

    Examples with tweened and fly transition

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,43 @@
 <script>
 	import reducedMotion from './reducedMotion';
+	import { fly } from 'svelte/transition';
+	import { tweened } from 'svelte/motion';
 
-	export let name;
+	let showCards = false;
+	const progress = tweened(0);
+
+	function toggleCards() {
+		showCards = !showCards;
+	}
+
+	function toggleProgress() {
+		progress.update((current) => (current === 0 ? 100 : 0), {
+			duration: $reducedMotion ? 0 : 400
+		});
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<p>reduced motion: {$reducedMotion}</p>
+
+	<button on:click={toggleCards}>Toggle cards </button>
+	<button on:click={toggleProgress}>Toggle progress</button>
+	<div>
+		Progress: <progress value={$progress} max="100" />
+	</div>
+
+	{#if showCards}
+		<div class="card-container">
+			{#each new Array(5) as _, i}
+				<div
+					transition:fly={{ y: 100, delay: $reducedMotion ? 0 : i * 100 }}
+					class="card"
+				>
+					Card {i + 1}
+				</div>
+			{/each}
+		</div>
+	{/if}
 </main>
 
 <style>
@@ -18,16 +48,26 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	main > * + * {
+		margin-top: 1em;
 	}
 
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
+	}
+
+	.card-container {
+		display: flex;
+	}
+
+	.card {
+		height: 150px;
+		width: 200px;
+		border: 2px solid black;
+		display: grid;
+		place-content: center;
+		background: lightskyblue;
 	}
 </style>
--- src/reducedMotion.js
+++ src/reducedMotion.js
@@ -20,5 +20,5 @@ export default readable(false, (set) => {
 
 	return () => {
 		mediaQueryList.removeEventListener('change', updateMotionPreference);
-	}
+	};
 });
